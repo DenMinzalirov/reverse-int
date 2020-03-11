d2d6dcfe1a577d92850e60657f88a17dccde1e66
Den Minzakirov <Den.Minzakirov@gmail.com>
2020-03-11 12:10:32 +0300

    feat: add inpliment reverse numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let res = (""+n).split("");
+    if (res[0] === '-') {
+        return res.slice(1).reverse().join('')
+    }
+    return res.reverse().join('')
 }
